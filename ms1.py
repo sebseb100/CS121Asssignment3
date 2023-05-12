a549ebc3dcb3494f637881532f1210ab9f01874a
Ben Boben <bboben@uci.edu>
2023-05-11 18:22:52 -0700

    fixed path

--- ms1.py
+++ ms1.py
@@ -6,15 +6,21 @@ from collections import defaultdict
 
 json_file = os.path.join(os.getcwd(), 'DEV')
 
+file_counter = 0
+index_size = 0
 
 def build_inverted_index():
+    global file_counter, index_size
+
     inverted_index = defaultdict(list)
 
     for cur, subdir, files in os.walk(json_file):
         for file in files:
+            print(file)
             if file.endswith('.json'):
-                with open(os.path.join(cur, file),'r') as fizzy:
-                    data = json.load(fizzy)
+                cur_file_path = os.path.join(cur, file)
+                with open(cur_file_path,'r') as f:
+                    data = json.load(f)
                 soup = BeautifulSoup(data['content'], 'html.parser')
                 text = soup.get_text()
 
@@ -22,18 +28,28 @@ def build_inverted_index():
                 undup_tokens = set(tokens)
                 for token in undup_tokens:
                     inverted_index[token].append({'doc_id': file, "tf-dif": tokens.count(token)})
+                file_counter += 1
+
+                index_size_bytes = os.path.getsize(cur_file_path)
+                index_size += (index_size_bytes/1024)
+
     with open('inverted_index.json', 'w') as f:
         json.dump(inverted_index, f)
     return inverted_index
 
 def write_report(inverted_dic):
+    global file_counter, index_size
     with open('report.txt', 'w') as f:
         f.write("Inverted Index\n\n")
-        for term, posts in inverted_dic.items():
-            f.write(f'Term: {term}\nDocuments:\n')
-            for post in posts:
-                f.write(f' - {post["doc_id"]}: tf={post["tf-dif"]}\n')
-            f.write('\n')
+        # for term, posts in inverted_dic.items():
+        #     f.write(f'Term: {term}\nDocuments:\n')
+        #     for post in posts:
+        #         f.write(f' - {post["doc_id"]}: tf={post["tf-dif"]}\n')
+        #     f.write('\n')
+        f.write(f'Number of Indexed Documents: {file_counter}\n')
+        f.write(f'Number of Unique Tokens: {len(inverted_dic.keys())}\n')
+        f.write(f'Total Size (kB): {index_size}\n')
+
 
 
 if __name__ == "__main__":
