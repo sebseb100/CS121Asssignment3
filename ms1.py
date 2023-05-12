5d0e13b2fa4cc9817aabd83271362aee6a7f6f88
Ben Boben <bboben@uci.edu>
2023-05-11 18:00:10 -0700

    changes to dictionary method

--- ms1.py
+++ ms1.py
@@ -2,29 +2,28 @@ import os
 import json 
 from bs4 import BeautifulSoup
 import re 
+from collections import defaultdict
 
-json_file = '/DEV'
+json_file = os.path.join(os.getcwd(), 'DEV')
 
 
 def build_inverted_index():
-    inverted_index = {}
+    inverted_index = defaultdict(list)
 
     for cur, subdir, files in os.walk(json_file):
-        print("HAHAHAHAH")
         for file in files:
             if file.endswith('.json'):
-                with open(os.path.join(cur,file),'r') as fizzy:
+                with open(os.path.join(cur, file),'r') as fizzy:
                     data = json.load(fizzy)
-
-                soup = BeautifulSoup(data['html'], 'html.parser')
+                soup = BeautifulSoup(data['content'], 'html.parser')
                 text = soup.get_text()
 
                 tokens = re.findall(r"[a-zA-Z0-9]+", text.lower())
                 undup_tokens = set(tokens)
                 for token in undup_tokens:
-                    if token not in inverted_index.keys():
-                        inverted_index[token] = []
                     inverted_index[token].append({'doc_id': file, "tf-dif": tokens.count(token)})
+    with open('inverted_index.json', 'w') as f:
+        json.dump(inverted_index, f)
     return inverted_index
 
 def write_report(inverted_dic):
@@ -33,12 +32,10 @@ def write_report(inverted_dic):
         for term, posts in inverted_dic.items():
             f.write(f'Term: {term}\nDocuments:\n')
             for post in posts:
-                f.write(f' - {post["doc-id"]}: tf={post["tf-dif"]}\n')
+                f.write(f' - {post["doc_id"]}: tf={post["tf-dif"]}\n')
             f.write('\n')
 
 
 if __name__ == "__main__":
-    print("HERE")
     inverted = build_inverted_index()
-    print("HERE2")
     write_report(inverted)
